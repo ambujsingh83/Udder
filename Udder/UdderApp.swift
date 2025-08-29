7eb313a49f7edf875d725fcd4fc73a5f26b28512
ambujsingh83 <ambujsingh83@gmail.com>
2025-08-29 15:44:49 +0530

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Ambuj Singh on 29/08/25.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UdderTests/UdderTests.swift
@@ -0,0 +1,17 @@
+//
+//  UdderTests.swift
+//  UdderTests
+//
+//  Created by Ambuj Singh on 29/08/25.
+//
+
+import Testing
+@testable import Udder
+
+struct UdderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
